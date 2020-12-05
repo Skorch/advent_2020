444d195f965d3373984aeb3dc9a24361cf7c4089
Drew Beaupre <drew@mammothgrowth.com>
2020-12-04 22:28:10 -0800

    part 1 of day 5

---
+++ day5.py
@@ -0,0 +1,29 @@
+import io
+
+input_file = 'day5.input'
+
+total_rows = 128
+total_cols = 7
+
+def fetch_row(start, end, code, low_code):
+
+    half = int((end-start) / 2)
+
+    next_start, next_end = (start, start + half) if code[0:1] == low_code else (end - half, end)
+
+    if len(code) > 1:
+        return fetch_row(next_start, next_end, code[1:], low_code)
+    else:
+        return next_start
+
+
+def process_seat_id(line):
+    x1 = fetch_row(0, total_rows-1, line[:7], "F")
+    y1 = fetch_row(0, total_cols-1, line[7:], "L")
+    print(f"row {x1} seat {y1} = {x1*8 + y1}")
+    return (x1*8) + y1
+
+with io.open(input_file, 'rt') as f:
+
+    answer = max([process_seat_id(line) for line in f.readlines()])
+    print(answer)
